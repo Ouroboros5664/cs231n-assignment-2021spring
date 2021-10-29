18bb11d52b97712eb6ded41464daa5b648e9265f
Junyi chen <jy.chen53@outlook.com>
2021-10-29 13:38:30 +0800

    finish knn

--- assignment1/test.py
+++ assignment1/test.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-x = np.array([[1,2],[3,4]])
+a = [1,2,3] * 3
 
-tmp = (x**2).sum(axis=1, keepdims=True).T
+a[0] = 0
 
-y = np.array([[1,2],[3,4]])
-
-print(tmp.shape, tmp)
-
-print(tmp + y)
+print(a)
